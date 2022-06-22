7e47097c80b4f0b9b03d94d83488d4f1850f23cc
Vladimir Lekhterev <vovalekhterev@gmail.com>
2022-06-22 14:49:53 +0300

    Add demo control script

--- build/robokit_webots/build/lib/robokit_webots/demo_head_control.py
+++ build/robokit_webots/build/lib/robokit_webots/demo_head_control.py
@@ -1,73 +1,34 @@
-# ROS Client Library for Python
 import rclpy
- 
-# Handles the creation of nodes
+from math import sin
 from rclpy.node import Node
- 
-# Enables usage of the String message type
-from std_msgs.msg import String
- 
+from std_msgs.msg import Float64MultiArray
+
 class MinimalPublisher(Node):
-  """
-  Create a MinimalPublisher class, which is a subclass of the Node class.
-  """
   def __init__(self):
-    """
-    Class constructor to set up the node
-    """
-    # Initiate the Node class's constructor and give it a name
-    super().__init__('minimal_publisher')
-     
-    # Create the publisher. This publisher will publish a String message
-    # to the addison topic. The queue size is 10 messages.
-    self.publisher_ = self.create_publisher(String, 'addison', 10)
-     
-    # We will publish a message every 0.5 seconds
-    timer_period = 0.5  # seconds
-     
-    # Create the timer
+
+    super().__init__('demo_head_control')
+    self.publisher_ = self.create_publisher(Float64MultiArray, '/rk_joint_group_position_controller/commands', 10)
+
+    timer_period = 0.5
     self.timer = self.create_timer(timer_period, self.timer_callback)
-  
-    # Initialize a counter variable
-    self.i = 0
- 
+    self.x = 0    
+
   def timer_callback(self):
-    """
-    Callback function.
-    This function gets called every 0.5 seconds.
-    """
-    # Create a String message
-    msg = String()
- 
-    # Set the String message's data
-    msg.data = 'Hi Automatic Addison: %d' % self.i
-     
-    # Publish the message to the topic
+    msg = Float64MultiArray()
+    msg.data = [.0]*23
+    msg.data[0] = sin(self.x)*0.5
+    msg.data[1] = sin(self.x)*0.5
+    print(msg.data)
     self.publisher_.publish(msg)
-     
-    # Display the message on the console
-    self.get_logger().info('Publishing: "%s"' % msg.data)
- 
-    # Increment the counter by 1    
-    self.i += 1
+    self.x+=1
  
 def main(args=None):
- 
-  # Initialize the rclpy library
+
   rclpy.init(args=args)
- 
-  # Create the node
   minimal_publisher = MinimalPublisher()
- 
-  # Spin the node so the callback function is called.
   rclpy.spin(minimal_publisher)
- 
-  # Destroy the node explicitly
-  # (optional - otherwise it will be done automatically
-  # when the garbage collector destroys the node object)
+
   minimal_publisher.destroy_node()
- 
-  # Shutdown the ROS client library for Python
   rclpy.shutdown()
  
 if __name__ == '__main__':
