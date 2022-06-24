87f9aaaa829074cfb36ee6c2f59f8c449f1b9a3b
Vladimir Lekhterev <vovalekhterev@gmail.com>
2022-06-24 12:28:34 +0300

    Add FileNotFoundError

--- src/robokit_webots/utils/parse_utils.py
+++ src/robokit_webots/utils/parse_utils.py
@@ -7,8 +7,10 @@ def get_joint_names(filename='joint_names.yaml'):
     DESCRIPTION_PATH = os.environ['ROBOKIT_DESCRIPTION']
     joint_names = None
     with open(f'{DESCRIPTION_PATH}/config/{filename}') as yaml_file:
-        yaml_content = yaml.load(yaml_file.read(), Loader=yaml.Loader)
-        joint_names = yaml_content['controller_joint_names']
+        try:
+            yaml_content = yaml.load(yaml_file.read(), Loader=yaml.Loader)
+            joint_names = yaml_content['controller_joint_names']
+        except FIleNotfoundError:
+            print("Error: joint names file not found")
     return joint_names
 
-print(get_joint_names())
